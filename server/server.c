742f8a1cee92b34b33c8a4034ba19629d9bc6e2f
Rinesa53 <rinesa.kurtaliqi@student.uni-pr.edu>
2026-04-17 21:04:04 +0200

    struktura dhe konstantet

--- server/server.c
+++ server/server.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <time.h>
 
+#define PORT 8080
+#define MAX_CLIENTS 5
+#define BUFFER_SIZE 1024
+#define TIMEOUT 30
+
+typedef struct {
+    int fd;
+    struct sockaddr_in addr;
+    time_t last_active;
+    int active;
+} Client;
